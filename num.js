8b1ec9b461a65a021c75bab3278a749b3b9cb570
Tusharborgaonkar <105264709+Tusharborgaonkar@users.noreply.github.com>
2022-08-17 09:54:52 +0530

    Add files via upload

---
+++ num.js
@@ -0,0 +1,17 @@
+var num=100;
+var str="tushar"
+var booleanvar=true;
+
+document.write("<br/>",num);
+document.write("<br/>",str);
+document.write("<br/>",booleanvar);
+
+document.write("<br/>",typeof(booleanvar));
+
+var obj = new Object();
+var obj ={
+    FirstName: "king",
+    lastname: "game of king",
+}
+document.write("<br/>"+typeof(obj));
+document.write("<br/>"+obj.FirstName+"<br/>"+obj.lastname);
